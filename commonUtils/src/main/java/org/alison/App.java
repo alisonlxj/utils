baf66ed5704fa48228fde6629d5baa87c1050821
lixianjun02 <lixianjun02@meituan.com>
2020-07-28 22:28:11 +0800

    initial proj

---
+++ commonUtils/src/main/java/org/alison/App.java
@@ -0,0 +1,13 @@
+package org.alison;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
